5bb8638466d5ad128db6fb7fb3b0cfc409e8ebe0
meesam4687 <meesam4687@gmail.com>
2024-10-14 11:22:49 +0530

    fix: i am retarded

--- scripts/index.js
+++ scripts/index.js
@@ -1,62 +1,126 @@
-const elements = document.querySelectorAll('.shape');
-const min = 1;
-const max = 1.5;
-let shapes;
+function expandElement(id, height) {
+  const element = document.getElementById(id);
+  element.style.height = height;
+  element.style.width = height;
+}
 
-const containerBox = {
-  x: window.innerWidth / 3,
-  y: window.innerHeight / 3,
-  width: window.innerWidth / 3,
-  height: window.innerHeight / 3
-};
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-class Shape {
-  constructor(el) {
-    this.el = el;
-    this.size = el.offsetWidth;
-    this.x = random(containerBox.x, containerBox.x + containerBox.width - this.size);
-    this.y = random(containerBox.y, containerBox.y + containerBox.height - this.size);
-    this.vx = random(min, max);
-    this.vy = random(min, max);
-  }
+function changeColor(color) {
+  fetch('./assets/googleCircle.svg')
+    .then(response => response.text())
+    .then(svgContent => {
+      const parser = new DOMParser();
+      const svgDoc = parser.parseFromString(svgContent, 'image/svg+xml');
+      const mainShape = svgDoc.querySelector('path');
+      const newColor = color;
+      mainShape.setAttribute('fill', newColor);
+      const serializer = new XMLSerializer();
+      const newSvgContent = serializer.serializeToString(svgDoc);
+      const img = document.getElementById('statusImage');
+      img.src = `data:image/svg+xml;base64,${btoa(newSvgContent)}`;
+    });
+  document.getElementById("circleOverSvg").style.backgroundColor = color;
+}
 
-  boundary() {
-    if (this.x >= containerBox.x + containerBox.width - this.size) {
-      this.vx *= -1;
-      this.x = containerBox.x + containerBox.width - this.size;
-    }
-    if (this.y >= containerBox.y + containerBox.height - this.size) {
-      this.vy *= -1;
-      this.y = containerBox.y + containerBox.height - this.size;
-    }
-    if (this.x <= containerBox.x) {
-      this.vx *= -1;
-      this.x = containerBox.x;
-    }
-    if (this.y <= containerBox.y) {
-      this.vy *= -1;
-      this.y = containerBox.y;
-    }
-  }
+function setListening() {
+  expandElement("circleOverSvg", "25rem");
+  document.getElementById("statusText").innerHTML = "Listening...";
+  document.getElementById("circleOverSvg").style.animationName = "none";
+  changeColor("#C6EEFF");
+  document.getElementById("statusImage").style.animationDuration = "10s"
+}
 
-  animate() {
-    this.x += this.vx;
-    this.y += this.vy;
-    this.el.style.transform = `translate(${this.x}px,${this.y}px)`;
-    this.boundary();
+async function setProcessing() {
+  expandElement("circleOverSvg", "30rem");
+  document.getElementById("statusText").innerHTML = "Processing...";
+  document.getElementById("circleOverSvg").style.animationName = "processing";
+  await sleep(500)
+  document.getElementById("statusImage").style.animationDuration = "25s"
+  changeColor("#87b6ff");
+}
+
+function setSpeaking() {
+  document.getElementById("statusImage").style.animationDuration = "25s"
+  expandElement("circleOverSvg", "25rem");
+  document.getElementById("statusText").innerHTML = "Speaking...";
+  document.getElementById("circleOverSvg").style.animationName = "none";
+  changeColor("#c9f2cb");
+}
+
+async function getInfo() {
+  let d;
+  await fetch(`${backendUrl}/status`)
+    .then(response => response.json())
+    .then(data => {
+      d = data;
+  });
+  console.log(d)
+  if(d.status == "listening") {
+    setListening();
+  } else if(d.status == "processing") {
+    setProcessing();
+  } else if(d.status == "speaking") {
+    setSpeaking();
   }
 }
 
-const random = (min, max) => Math.random() * (max - min) + min;
+async function getResources(){
+  let d;
+  await fetch(`${backendUrl}/resources`)
+    .then(response => response.json())
+    .then(data => {
+      d = data;
+  });
+  setProgressCpu(d.cpu);
+  setProgressRam(d.ram);
+}
+getResources()
+setInterval(getResources, 5000);
 
-function update() {
-  shapes.forEach((shape) => shape.animate());
-  requestAnimationFrame(update);
+//setInterval(getInfo, 1000);
+
+const circleCpu = document.getElementById("cpuUsage")
+const percentageDisplayCpu = document.getElementById("cpuPercent")
+const circumference = 92 * 2 * Math.PI;
+circleCpu.style.strokeDasharray = circumference;
+const circleRam = document.getElementById("ramUsage")
+const percentageDisplayRam = document.getElementById("ramPercent")
+circleRam.style.strokeDasharray = circumference;
+setProgressCpu(0)
+setProgressRam(0)
+
+function setProgressRam(percent) {
+  const offset = circumference - (percent / 100 * circumference);
+  circleRam.style.strokeDashoffset = offset;
+  percentageDisplayRam.innerHTML = percent.toString().split(".")[0] + '%';
+}
+function setProgressCpu(percent) {
+  const offset = circumference - (percent / 100 * circumference);
+  circleCpu.style.strokeDashoffset = offset;
+  percentageDisplayCpu.innerHTML = percent.toString().split(".")[0] + '%';
 }
 
-function init() {
-  shapes = Array.from(elements, (el) => new Shape(el));
-  update();
-}
-
-window.addEventListener('load', init, false);
+fetch(weatherAPIEndpoint).then(response => response.json()).then(data => {
+  let temperature = data.current.temp_c.toString().split(".")[0];
+  let condition = data.current.condition.text
+  if(sunnyConditions.includes(condition)) {
+    document.getElementById("weatherIcon").src = "./assets/sunny.svg";
+  } else if(cloudyConditions.includes(condition)) {
+    document.getElementById("weatherIcon").src = "./assets/cloudy.svg";
+  } else if(partlyCloudyConditions.includes(condition)) {
+    document.getElementById("weatherIcon").src = "./assets/partlyCloudy.svg";
+  } else if(mistConditions.includes(condition)) {
+    document.getElementById("weatherIcon").src = "./assets/mist.svg";
+  } else if(rainConditions.includes(condition)) {
+    document.getElementById("weatherIcon").src = "./assets/rain.svg";
+  } else if(thunderConditions.includes(condition)) {
+    document.getElementById("weatherIcon").src = "./assets/thunder.svg";
+  } else if(snowConditions.includes(condition)) {
+    document.getElementById("weatherIcon").src = "./assets/snow.svg";
+  }
+  document.getElementById("temperature").innerHTML = temperature + "°C";
+  document.getElementById("condition").innerHTML = condition;
+});
